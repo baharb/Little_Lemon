e628d80a544b204e28927a88cd2b24b0b3addf6d
Bahareh <bahar.bahreman@gmail.com>
2024-07-23 19:44:44 -0700

    add index and base menu

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      HomePage
-    </div>
+    <>
+      <header><image src='logo.png' /></header>
+        <nav>
+          <ul>
+            <li><a href='#'>Home</a></li>
+            <li><a href='#'>About</a></li>
+            <li><a href='#'>Menu</a></li>
+            <li><a href='#'>Order online</a></li>
+            <li><a href='#'>Reservations</a></li>
+            <li><a href='#'>Login</a></li>
+          </ul>
+        </nav>
+      <main></main>
+      <footer>
+        <p>Copyright</p>
+      </footer>
+    </>
   );
 }
 
