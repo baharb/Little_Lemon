7b43002af0c22b370275b0d975248d66c7d84b3b
Bahareh <bahar.bahreman@gmail.com>
2024-08-08 23:35:05 -0700

    Styling About section

--- src/components/About.js
+++ src/components/About.js
@@ -1,22 +1,33 @@
 import React from "react";
-import {Box, Grid, GridItem} from "@chakra-ui/react";
+import {Box, Button, Grid, GridItem, Image} from "@chakra-ui/react";
+import "../styles/about.css";
+
+import restaurant1 from '../assets/restaurantchefB.jpg';
+import restaurant2 from '../assets/restaurant.jpg';
 
 const About = () => {
-  return (
-    <Box backgroundColor="#18181b">
-      <about>
-      <Grid
-        templateRows='repeat(3, 1fr)'
-        templateColumns='repeat(2, 1fr)'
+  return (    
+      <div className="about" >
+        <div className="containerAbout" >        
+          <div className="aboutText">
+            <h1 className="titleBigAbout">Little Lemon</h1>
+            <h2 className="subTitleAbout">Chicago</h2>
+            <h6 className="textAbout">
+              We are a family owned mediterranean restaurant, focused on traditional recipes served with a modern twist. 
+              Nestled in the heart of bustling Chicago, Little Lemon is where modern 
+              flair meets cozy nostalgia. Our diverse, artisanal menu.
+              Whether you're seeking a romantic dinner, a family meal, or a relaxed atmosphere with friends, Little Lemon is your ideal culinary destination. 
+              Find us in the heart of Chicago for an unforgettable experience.
+             </h6>
+             </div>
+          <div>
+          <Image  className="aboutImg img2" src={restaurant2}  />
+            <Image  className="aboutImg img1" src={restaurant1}  />
+          </div>
+        </div>
         
-      >
-        <GridItem rowSpan={2} colSpan={1} bg='#495E57' />
-        <GridItem rowSpan={2} colSpan={1} bg='#495E57' />
-        <GridItem rowSpan={1} colSpan={1} bg='green' />
-        <GridItem rowSpan={1} colSpan={1} bg='black' />
-      </Grid>
-      </about>
-    </Box>
+      </div>
+    
   );
 };
 export default About;
